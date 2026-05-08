b03b9530d49832b189757a5b2d5ef2be55134220
r3als1l3nc3 <134794463+r3als1l3nc3@users.noreply.github.com>
2026-05-08 16:41:10 -0400

    Enable admin password middleware

---
+++ middleware.ts
@@ -0,0 +1,35 @@
+import { NextRequest } from "next/server";
+
+export const config = {
+  matcher: ["/admin/:path*"],
+};
+
+export function middleware(req: NextRequest) {
+  const username = process.env.ADMIN_USERNAME || "admin";
+  const password = process.env.ADMIN_PASSWORD;
+
+  if (!password) {
+    return new Response("Admin password is not set.", { status: 500 });
+  }
+
+  const authHeader = req.headers.get("authorization");
+
+  if (authHeader) {
+    const basicAuth = authHeader.split(" ")[1];
+    const [user, pass] = atob(basicAuth).split(":");
+
+    if (user === username && pass === password) {
+      return;
+    }
+  }
+
+  return new Response("Authentication required.", {
+    status: 401,
+    headers: {
+      "WWW-Authenticate": 'Basic realm="TrifeBeats Admin"',
+    },
+  });
+}
+export const config = {
+  matcher: ["/admin/:path*"],
+}
